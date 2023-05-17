d5f6fb75bff460cf13647655b587237d54530da2
Daniel Velasco <danilovelga4747@gmail.com>
2023-05-17 16:48:55 -0500

    enable cors and add environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'danielvelasco1234',
-      database: 'nestjs-typeorm',
+      host: AppModule.host,
+      port: AppModule.port,
+      username: AppModule.username,
+      password: AppModule.password,
+      database: AppModule.database,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
@@ -23,4 +25,18 @@ import { PostsModule } from './posts/posts.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static host: string;
+  static port: number;
+  static username: string;
+  static password: string;
+  static database: string;
+
+  constructor(private readonly configService: ConfigService) {
+    AppModule.host = this.configService.get('DB_HOST');
+    AppModule.port = +this.configService.get('DB_PORT');
+    AppModule.username = this.configService.get('DB_USERNAME');
+    AppModule.password = this.configService.get('DB_PASSWORD');
+    AppModule.database = this.configService.get('DB_DATABASE');
+  }
+}
